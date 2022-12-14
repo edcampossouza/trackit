77698fe944925b7671e658184a1d1e1a2601e1f7
Eduardo Souza <edcampossouza@gmail.com>
2022-12-14 17:43:58 -0300

    login functionality

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,65 @@
+import axios from "axios";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import logo from "../assets/logo.png";
+import { URL } from "../constants";
 import { ContainerStyle } from "../styles/AuthPages";
+import Dots from "./Dots";
 
 export default function Login() {
   const navigate = useNavigate();
+  const [loginInfo, setLoginInfo] = useState({ password: "", email: "" });
+  const [loading, setLoading] = useState(false);
+
+  function onChange(e) {
+    const { name, value } = e.target;
+    setLoginInfo((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  }
+
+  function userLogin(e) {
+    e.preventDefault();
+    setLoading(true);
+    axios
+      .post(`${URL}/auth/login`, loginInfo)
+      .then((res) => {
+        console.log(res.data);
+        setLoading(false);
+        navigate("/hoje");
+      })
+      .catch((err) => {
+        const msg =
+          err.response.data.message || JSON.stringify(err.response.data);
+        alert("Erro: " + msg);
+        setLoading(false);
+      });
+  }
   return (
     <ContainerStyle>
       <img src={logo} alt="site-logo" />
-      <input placeholder="email" />
-      <input placeholder="senha" />
-      <button onClick={() => navigate("/habitos")}>Entrar</button>
+      <form onSubmit={userLogin}>
+        <input
+          placeholder="email"
+          name="email"
+          value={loginInfo.email}
+          type="email"
+          required
+          onChange={onChange}
+          disabled={loading}
+        />
+        <input
+          placeholder="senha"
+          name="password"
+          value={loginInfo.password}
+          type="password"
+          required
+          onChange={onChange}
+          disabled={loading}
+        />
+        <button type="submit">{loading ? <Dots /> : "Entrar"}</button>
+      </form>
       <a href="/cadastro">Não tem uma conta? Cadastre-se!</a>
     </ContainerStyle>
   );
